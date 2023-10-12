e750969ab38b151f7414f5fbe1db93ba48123298
udaybisht124 <bishtuday4668@gmail.com>
2023-10-12 18:43:40 +0530

    33chane

--- program33.java
+++ program33.java
@@ -1,15 +1,9 @@
 import java.awt.*;
-class a {
-    public static void main(String[] args) {
-        Frame f1= new Frame("youtube");
-        Button b1 = new Button("ok");
-        Button b2 = new Button("byee");
-        Button b3 = new Button("bobby");
-        f1.setLayout(null);
+class  f1.setLayout(null);
         f1.add(b1);
         f1.add(b2);
         f1.add(b3);
-        b1.setBounds(550, 200, 100, 400);
+        b1.setBounds(550, 200, 100, 500);
     b2.setBounds(650, 200, 100, 400);
     b3.setBounds(750, 200, 100, 400);
         b1.setBackground(Color.cyan);
@@ -17,6 +11,12 @@ class a {
 b3.setBackground(Color.pink);
         f1.setBackground(Color.black);
 
-        f1.setVisible(true);
+        f1.setVisible(true); a {
+    public static void main(String[] args) {
+        Frame f1= new Frame("youtube");
+        Button b1 = new Button("ok");
+        Button b2 = new Button("byee");
+        Button b3 = new Button("bobby");
+      
     }
 }
