7496db8e06e726ec099f6a7b2e52d6ae2366a78e
udaybisht124 <bishtuday4668@gmail.com>
2023-10-13 16:43:00 +0530

    program33done

--- program33.java
+++ program33.java
@@ -1,22 +1,22 @@
-import java.awt.*;
-class  f1.setLayout(null);
-        f1.add(b1);
-        f1.add(b2);
-        f1.add(b3);
-        b1.setBounds(550, 200, 100, 500);
-    b2.setBounds(650, 200, 100, 400);
-    b3.setBounds(750, 200, 100, 400);
-        b1.setBackground(Color.cyan);
-        b2.setBackground(Color.yellow);
-b3.setBackground(Color.pink);
-        f1.setBackground(Color.black);
+import java.util.Scanner;
 
-        f1.setVisible(true); a {
+ class a {
     public static void main(String[] args) {
-        Frame f1= new Frame("youtube");
-        Button b1 = new Button("ok");
-        Button b2 = new Button("byee");
-        Button b3 = new Button("bobby");
-      
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the length of the prism:");
+        double length = scanner.nextDouble();
+
+        System.out.println("Enter the width of the prism:");
+        double width = scanner.nextDouble();
+
+        System.out.println("Enter the height of the prism:");
+        double height = scanner.nextDouble();
+
+        double area = length * width;
+        double volume = area * height;
+
+        System.out.println("The area of the prism is: " + area);
+        System.out.println("The volume of the prism is: " + volume);
     }
 }
