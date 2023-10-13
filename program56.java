11f60c04cb41f4dc5fe50b2d9605d8d6b2bce31c
udaybisht124 <bishtuday4668@gmail.com>
2023-10-13 17:43:39 +0530

    program56done

---
+++ program56.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class a{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the Year");
+    int year =sc.nextInt();
+  if((year % 400 == 0) ||(year % 100 != 0) && (year % 4 == 0)){
+    System.out.println("leap year");
+  }
+  else{
+    System.out.println("Not a Leap Year");
+  }
+
+}
+}
