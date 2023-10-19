eb915916d036153903cd42e407d6e17d37176605
udaybisht124 <bishtuday4668@gmail.com>
2023-10-19 19:23:51 +0530

    doneagain

--- program56.java
+++ program56.java
@@ -1,19 +1,16 @@
-//import java.util.Scanner;
-class a{
+import java.util.Scanner;
+class pro56{
     public static void main(String[] args) {
-//         Scanner sc = new Scanner(System.in);
-//         System.out.println("enter the Year");
-//     int year =sc.nextInt();
-//   if((year % 400 == 0) ||(year % 100 != 0) && (year % 4 == 0)){
-//     System.out.println("leap year");
-//   }
-//   else{
-//     System.out.println("Not a Leap Year");
-//   }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the Year");
+    int year =sc.nextInt();
+  if((year % 400 == 0) ||(year % 100 != 0) && (year % 4 == 0)){
+    System.out.println("leap year");
+  }
+  else{
+    System.out.println("Not a Leap Year");
+  }
 
-// }
+}
     
-String str = "uday";
-System.out.println(str.replace("a", "i"));
-}
-}
+    }
