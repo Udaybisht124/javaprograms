c616c9d38adfe705d5123d8ed0ba6fd910bd3875
udaybisht124 <bishtuday4668@gmail.com>
2023-10-19 19:13:40 +0530

    done

--- program17.java
+++ program17.java
@@ -1,4 +1,4 @@
-class a{
+class pro17{
     public static void main(String[] args) {
     System.out.println("program to enter the even number 1 to 20\n");
 for(int i=0;i<=20;i++){
--- program35.java
+++ program35.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
- class a {
+ class pro35{
     public static void main(String[] args) {
        
         Scanner scanner = new Scanner(System.in);
